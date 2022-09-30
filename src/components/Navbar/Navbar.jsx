4b5f0a804b7390a9e8b17bf628070166743f8798
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-09-30 23:05:11 +0100

    created navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Icon } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/shopnow.png';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position='fixed' className={classes.appBar} color='inherit'>
+                <Toolbar>
+                    <Typography variant='h6' className={classes.title} color='inherit'>
+                        <img src={logo} alt="Shopnow" height='25px' className={classes.image} />
+                        Shopnow Store
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label='Show cart items' color='inherit'>
+                            <Badge badgeContent={2} color='secondary'></Badge>
+                            <ShoppingCart />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
