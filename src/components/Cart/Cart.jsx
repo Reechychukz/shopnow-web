385b1c5d18898db5677259d0b4cc4fad77fa326b
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-12-04 18:37:16 +0100

    created cart item component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
     const classes = useStyles();
@@ -16,12 +17,12 @@ const Cart = ({ cart }) => {
                 <Grid container spacing={3}>
                     {cart.line_items.map((item) => (
                         <Grid item xs={12} sm={4} key={item.id}>
-                            <div>{item.name}</div>
+                            <CartItem item={item} />
                         </Grid>
                     ))}
                 </Grid>
                 <div className={classes.cardDetails}>
-                    <Typography variant='h4'>SubTotal: {cart.subTotal?.formatted_with_symbol}</Typography>
+                    <Typography variant='h4'>SubTotal: {cart.subtotal.formatted_with_symbol}</Typography>
                     <div>
                         <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'> Empty Cart</Button>
                         <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'> Checkout</Button>
@@ -35,7 +36,7 @@ const Cart = ({ cart }) => {
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
             {cart.line_items.length ? <FilledCart /> : <EmptyCart />}
         </Container>
     )
